69f23677c4ee357f1dbf665946fe5a69dc8f9f26
pi-v <60312329+pi-v@users.noreply.github.com>
2023-01-11 15:43:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "tencentcloud_vpc" "foo" {
-  name         = TF_VAR_vpcname
-  cidr_block   = TF_VAR_ip
+  name         = var.vpcname
+  cidr_block   = var.ip
   dns_servers  = ["119.29.29.29", "8.8.8.8"]
   is_multicast = false
 
   tags = {
-    "test" = TF_VAR_tag
+    "test" = var.vpcname
   }
 }
